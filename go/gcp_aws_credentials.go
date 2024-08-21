fc3da9514111b3dc8b9e751ad9342fd0760327cc
Daniel Strebel <strebel@google.com>
2024-08-21 06:54:30 +0000

    feat: call vertex

--- go/gcp_aws_credentials.go
+++ go/gcp_aws_credentials.go
@@ -4,9 +4,13 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strings"
 
+	"cloud.google.com/go/vertexai/genai"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google/externalaccount"
+	"google.golang.org/api/option"
 )
 
 type customAwsSecurityCredentialsSupplier struct{}
@@ -34,6 +38,30 @@ func (s customAwsSecurityCredentialsSupplier) AwsSecurityCredentials(ctx context
 		SecretAccessKey: credentials.SecretAccessKey,
 		SessionToken:    credentials.SessionToken,
 	}, nil
+}
+
+func generateContent(prompt string, customTokenSource oauth2.TokenSource) (string, error) {
+	ctx := context.Background()
+
+	client, err := genai.NewClient(ctx, os.Getenv("GCP_PROJECT_ID"), "us-east1", option.WithTokenSource(customTokenSource))
+	if err != nil {
+		return "", err
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel("gemini-1.5-flash-001")
+	model.SetTemperature(0.8)
+
+	res, err := model.GenerateContent(ctx, genai.Text(prompt))
+	if err != nil {
+		return "", fmt.Errorf("unable to generate content: %v", err)
+	}
+
+	ret := make([]string, len(res.Candidates[0].Content.Parts))
+	for i, part := range res.Candidates[0].Content.Parts {
+		ret[i] = fmt.Sprintf("%v", part)
+	}
+	return strings.Join(ret, ""), nil
 }
 
 func main() {
@@ -72,12 +100,13 @@ func main() {
 		return
 	}
 
-	// Now you have a valid GCP token in `creds.TokenSource`
-	token, err := tokenSource.Token()
-	if err != nil {
-		fmt.Printf("Error obtaining token: %v\n", err)
-		return
-	}
+	// For Debug purposes only
+	// token, err := tokenSource.Token()
+	// if err != nil {
+	// 	fmt.Printf("Error obtaining token: %v\n", err)
+	// 	return
+	// }
+	// fmt.Printf("Access token: %s\n", token.AccessToken)
 
-	fmt.Printf("Access token: %s\n", token.AccessToken)
+	generateContent("Tell me a funny joke about food.", tokenSource)
 }
